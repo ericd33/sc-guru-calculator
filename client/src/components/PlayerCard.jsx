3a96310031c5bf058338bd7dae44c4066e53f7e3
Eric Daniele <ericdaniele34@gmail.com>
2023-02-20 00:10:34 -0300

    added price files

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -1,12 +1,23 @@
-import { Typography, Grid } from '@mui/material';
+import { Typography, Grid, Paper } from '@mui/material';
 import React from 'react'
 import './playerCard.css'
+const currency = require('currency.js')
+
 
 export default function PlayerCard({data}) {
 
   return (
     <Grid item mx='auto' className="playerCard" style={{backgroundImage: `url(${data.cardImg})`}}>
-      <Typography variant='h4' >{data.overallStats}</Typography>
+      <Grid container direction={'column'} >
+
+
+      <Paper className='playerPrice' sx={{backgroundColor: 'rgb(126, 218, 174)', textAlign:'center', fontWeight: 'bold'}} mx='auto'>
+      <Typography variant='h7' mx='auto' >{currency(data.buyCost, {fromCents: true, precision: 0}).format()}</Typography>
+      </Paper>
+
+      <Typography variant='h5' mx='auto' >{data.overallStats}</Typography>
+      </Grid>
+      
     </Grid>
   )
 }
